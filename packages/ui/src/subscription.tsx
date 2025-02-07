10fa192a7088dbdeb1da8a054e01885d59b1e40d
roni505 <161919194+roni505@users.noreply.github.com>
2025-02-08 01:18:14 +0530

    Subscription component changed

--- packages/ui/src/subscription.tsx
+++ packages/ui/src/subscription.tsx
@@ -2,12 +2,17 @@ import Button from "./button"
 
 const subscription = () => {
     return (
-        <div>
-            <p className="ui-text-white">
-            Receive Notifications for Every New Post
+        <div className="ui-flex ui-flex-col ui-items-center ui-justify-center ui-pt-52 ui-pb-52">
+            <div className="ui-text-white ui-text-5xl ui-text-center ui-font-semibold">
+            <p>
+            Receive Notifications for
             </p>
-            <div>
-            <input type="email" name="" id="" />
+            <p className="ui-mt-4">
+            Every New Post
+            </p>
+            </div>
+            <div className="ui-flex ui-space-x-4 ui-mt-8">
+            <input className="ui-bg-slate-900 ui-ps-20" type="email" name="" id="" placeholder="Your email address"/>
             <Button>Subscribe</Button>
             </div>
         </div>
