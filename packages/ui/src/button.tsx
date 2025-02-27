5a22879c456858e779992373476845f454437368
roni505 <161919194+roni505@users.noreply.github.com>
2025-02-27 22:30:49 +0530

    btn_UI_Changes

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -2,37 +2,39 @@ type ButtonProps = {
     text: string,
     variant: "primary" | "secondary",
     onClick?: () => void,
-    size: "sm" | "md" | "lg",
+    size?: "sm" | "md" | "lg",
     type?: "submit" | "reset" | "button",
     disabled?: boolean
 }
 
 const variantStyle = {
-    primary: "ui-bg-white ui-text-black",
+    primary: "ui-bg-dark ui-text-white ui-border-strokeColor ui-border",
     secondary: "ui-bg-gray-800 ui-text-gray-400"
 }
 
 const disabledStyle = "ui-opacity-50 ui-cursor-not-allowed";
 
 const sizeStyle = {
-    lg: "ui-px-4 ui-py-1.5 ui-rounded-sm ui-font-bold hover:ui-bg-slate-300",
+    lg: "ui-px-6 ui-py-4 ui-rounded-[14px]",
     md: "ui-px-2 ui-py-1.0 ui-rounded-sm ui-font-bold hover:ui-bg-slate-300",
     sm: "ui-px-1 ui-py-0.5 ui-rounded-sm ui-font-bold hover:ui-bg-slate-300"
 }
+
+const responsiveSize = "ui-text-base ui-px-4 ui-py-2 ui-lg:px-6 ui-lg:py-4 ui-lg:text-lg ui-text-base"
 
 const Button = (
     {
         text,
         variant,
         onClick,
-        size,
+        size = "md",
         type,
         disabled = false
     }: ButtonProps
 ) => {
     return (
         <button 
-        className={`${variantStyle[variant]} ${sizeStyle[size]} ${disabled ? disabledStyle : ""} `}
+        className={`${responsiveSize} ${variantStyle[variant]} ${sizeStyle[size]} ${disabled ? disabledStyle : ""} `}
         onClick={!disabled ? onClick : undefined}
         type={type}
         disabled={disabled}
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,10 +1,18 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "prefix" | "presets" | "content"> = {
+const config: Pick<Config, "prefix" | "presets" | "content" | "theme"> = {
   content: ["./src/**/*.tsx"],
   prefix: "ui-",
   presets: [sharedConfig],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#1F1F1F",
+        strokeColor: "#494949"
+      }
+    }
+  }
 };
 
 export default config;
