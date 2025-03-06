0d611feb622797ca866fcbdb721991eeb2c26ddc
roni505 <161919194+roni505@users.noreply.github.com>
2025-03-06 19:42:08 +0530

    Blogs Changed

--- packages/ui/src/allBlogs.tsx
+++ packages/ui/src/allBlogs.tsx
@@ -1,32 +1,47 @@
+"use client"; // Mark this as a Client Component
+
 import axios from "axios";
 import { CreateBlog } from "@repo/zod-schemas/validation";
 import Link from "next/link";
 import Blog from "./blog";
+import { useEffect, useState } from "react";
 
-const fetchBlogs = async () => {
-    const res = await axios.get("https://my-app.jyotimukherjeeadra86.workers.dev/api/blog/all-blogs");
-    const data = Object.values(res.data.blogs) as CreateBlog[];
-    return data;
-};
+const AllBlogs = () => {
+    const [blogs, setBlogs] = useState<CreateBlog[]>([]);
+    const [loading, setLoading] = useState(true);
 
-const AllBlogs = async () => {
-    const blogs = await fetchBlogs();    
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            try {
+                const res = await axios.get("https://my-app.jyotimukherjeeadra86.workers.dev/api/blog/all-blogs");
+                setBlogs(Object.values(res.data.blogs));
+            } catch (error) {
+                console.error("Error fetching blogs:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchBlogs();
+    }, []);
+
+    if (loading) return <p className="text-white">Loading blogs...</p>;
+
     return (
         <div>
-            {blogs.map((blog: CreateBlog) => {
-                return (
-                    <Link href={`/blogs/${blog.id}`} key={blog.id}>
-                        <div className="text-white px-5 sm:p-0 sm:max-w-2xl lg:max-w-2xl flex items-center mx-auto justify-center" key={blog.id}>
-                          <Blog title={blog.title} content={blog.content} publishedBy={blog.author?.name} date={blog.createdAt} />
-                        </div>
-                    </Link>
-                );
-            })}
+            {blogs.map((blog) => (
+                <Link href={`/blogs/${blog.id}`} key={blog.id}>
+                    <div className="text-white px-5 sm:p-0 sm:max-w-2xl lg:max-w-2xl flex items-center mx-auto justify-center">
+                        <Blog title={blog.title} content={blog.content} publishedBy={blog.author?.name} date={blog.createdAt} />
+                    </div>
+                </Link>
+            ))}
         </div>
     );
 };
 
 export default AllBlogs;
+
 
 
 
