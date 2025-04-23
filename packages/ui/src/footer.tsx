c5d83a15e2cb61034719a450461789861e5689ea
roni505 <161919194+roni505@users.noreply.github.com>
2025-04-23 23:57:30 +0530

    Footer UI changes

--- packages/ui/src/footer.tsx
+++ packages/ui/src/footer.tsx
@@ -1,24 +1,30 @@
 import Link from "next/link";
 
 const Footer = () => {
-    return (
-        <>
-        <div className="relative">
+  return (
+    <>
+      <div className="relative">
         <footer className="flex flex-col lg:max-w-7xl mx-auto justify-center">
-                <div className="p-12 text-xl lg:text-3xl text-center bg-gradient-to-r from-white to-[#000000] bg-clip-text text-transparent font-semibold">
-                    <h1>Designed and Built by Roni</h1>
-                </div>
-                <hr className="border border-[#282828] lg:w-[484px]  flex justify-center mx-auto mb-12"/>
-                <div className="text-[#5D5D5D] flex gap-14 pb-12 justify-center">
-                    <Link href="/Blogs" className="hover:text-[#979797]">Blogs</Link>
-                    <Link href="/create-account" className="hover:text-[#979797]">Get-started</Link>
-                    <Link href="/Login" className="hover:text-[#979797]">login</Link>
-                </div>
-            </footer>
-            <div className="absolute bottom-0 left-1/2 -translate-x-1/2 max-w-[100vw] w-full h-[300px] mx-auto z-[-1] shadow-[0px_-32px_62.6px_0px_#4A4A4A] bg-black rounded-t-[90%]"></div>
-        </div>
-        </>
-    );
+          <div className="p-12 text-xl lg:text-3xl text-center bg-gradient-to-r from-white to-[#000000] bg-clip-text text-transparent font-semibold">
+            <h1>Designed and Built by Roni</h1>
+          </div>
+          <hr className="border border-[#282828] lg:w-[484px]  flex justify-center mx-auto mb-12" />
+          <div className="text-[#5D5D5D] flex gap-14 pb-12 justify-center">
+            <Link href="/Blogs" className="hover:text-[#979797]">
+              Blogs
+            </Link>
+            <Link href="/create-account" className="hover:text-[#979797]">
+              Get-started
+            </Link>
+            <Link href="/Login" className="hover:text-[#979797]">
+              login
+            </Link>
+          </div>
+        </footer>
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 max-w-[100vw] w-full h-[300px] mx-auto z-[-1] shadow-[0px_-32px_62.6px_0px_#4A4A4A] bg-black rounded-t-[90%]"></div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
